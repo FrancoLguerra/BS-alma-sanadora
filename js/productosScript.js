da28d7a93078aa472996b86051958e462bc580b5
Franco Guerra <francolguerra@gmail.com>
2024-09-17 10:03:55 -0300

    Se agregan botones para agregar y descontar producto del carrito(aun no creado)

--- js/productosScript.js
+++ js/productosScript.js
@@ -7,3 +7,32 @@ selectNav1.addEventListener('change', function() {
     window.location.href = url;
     console.log('url', url);
 });
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    loadCart();
+});
+
+function addToCart(name, price, cant){
+
+    let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+    const product = {name,price,cant};
+    cart.push(product);
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+    loadCart();
+}
+
+function loadCart(){
+    let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+    let cartItems = document.getElementById('cartItem');
+    cartItems.innerHTML='';
+
+    cart.forEach(element => {
+        console.log(item.name, item.price, item.cant);
+        cartItems.innerHTML += `<p>${item.name} - $${item.price} - ${item.cant} <button onclick="removeFromCart(${index})">Eliminar</button></p>`
+    });
+    if (cart.length === 0) {
+        cartItems.innerHTML = '<p>El carrito está vacío</p>';
+      }
+}
+
